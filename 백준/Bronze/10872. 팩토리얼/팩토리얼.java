02e078abf83a449ed14b0325c815ca441e4b66d2
Hanhyungyu <143498466+Hanhyungyu@users.noreply.github.com>
2025-04-01 17:36:06 +0900

    [Bronze III] Title: 팩토리얼, Time: 172 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+Scanner scan=new Scanner(System.in);
+int x=scan.nextInt();
+        int a = 1;  
+        for (int i = 1; i <= x; i++) {
+            a = a * i;
+         
+              
+        }
+        System.out.println(a);
+
+
+
+    }
+}
