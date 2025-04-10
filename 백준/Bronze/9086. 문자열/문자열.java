5c40213f92d4cf6e0c29c4574b183e7e34023027
Hanhyungyu <143498466+Hanhyungyu@users.noreply.github.com>
2025-04-10 15:07:20 +0900

    [Bronze V] Title: 문자열, Time: 104 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[]args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int T=Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++){
+            String input=br.readLine();
+            System.out.print(input.charAt(0));
+            System.out.println(input.charAt(input.length()-1));
+        }
+    }
+}
