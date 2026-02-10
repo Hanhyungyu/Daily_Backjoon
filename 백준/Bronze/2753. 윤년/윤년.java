e0ca0e1b8e5b9fca873748e3bf15e2044c14b873
Hanhyungyu <143498466+Hanhyungyu@users.noreply.github.com>
2026-02-11 08:38:49 +0900

    [Bronze V] Title: 윤년, Time: 152 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,12 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+public class Main{
     public static void main(String[]args){
-   
-       Scanner scan=new Scanner(System.in);
-        int x=scan.nextInt();
-
-        if((x%4==0 && !(x%100==0))|| x%400==0){
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        if(((A%4)==0 &&(A%100)!=0)||(A%400)==0){
             System.out.println("1");
         }else{
             System.out.println("0");
